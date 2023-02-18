463eb968a7d65978a15fee3cec714f4194e745ec
kosintsev_ag <andrey.kosintsev@evraz.com>
2023-02-18 21:08:13 +0700

    Реализован бейзлайн прогнозного сервиса

---
+++ components/exhauster_backend/exhauster/application/predictor/settings.py
@@ -0,0 +1,8 @@
+from pydantic import BaseSettings, Field
+
+
+class Settings(BaseSettings):
+    VIBRATION_SENSITIVITY: float = 0.3
+    ROLLING_WINDOW_MINUTES: int = 300
+    RESAMPLE_WINDOW_MINUTES: int = 60
+    WARNING_VIBRATION_SENSITIVITY: float = 0
